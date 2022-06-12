666bba366f5a2f31931a474d7e2855692ebda15e
Dave Menninger <dave.menninger@gmail.com>
2022-06-12 08:29:43 -0400

    add application startup message

--- lib/ex_spring83/application.ex
+++ lib/ex_spring83/application.ex
@@ -2,6 +2,7 @@ defmodule ExSpring83.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   import Cachex.Spec
 
@@ -13,6 +14,8 @@ defmodule ExSpring83.Application do
       # cache ttl for boards is 28 days per Spring '83 spec
       {Cachex, name: :boards, expiration: expiration(default: :timer.hours(24) * 28)}
     ]
+
+    Logger.info("Starting application... http://localhost:4040/ ")
 
     opts = [strategy: :one_for_one, name: ExSpring83.Supervisor]
     Supervisor.start_link(children, opts)
